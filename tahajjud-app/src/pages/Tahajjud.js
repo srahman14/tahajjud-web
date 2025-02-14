4e9a880ea814e2f212ab856d19193024c2ba53d4
srahman14 <s.rahman14@hotmail.com>
2025-02-14 02:41:47 +0000

    inital commit

---
+++ tahajjud-app/src/pages/Tahajjud.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Tahajjud() {
+  return (
+    <div className="bg-blue-500 text-white min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">Tahajjud Page</h1>
+    </div>
+  );
+}
+
+export default Tahajjud;
