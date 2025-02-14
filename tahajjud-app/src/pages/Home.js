3c3b68f0ec5f40d010c328f05e89f25a43a935a6
srahman14 <s.rahman14@hotmail.com>
2025-02-14 02:41:26 +0000

    only prayertimes showing

--- tahajjud-app/src/pages/Home.js
+++ tahajjud-app/src/pages/Home.js
@@ -4,7 +4,6 @@ import PrayerTimes from '../components/prayertimes';
 function Home() {
   return (
     <div className="bg-blue-500 text-white min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold">Home Page</h1>
       <PrayerTimes />
     </div>
   );
