d7ffa6eb446069515fdb796a8a3c28422172dd7e
srahman14 <s.rahman14@hotmail.com>
2025-02-16 21:55:14 +0000

    misc

--- tahajjud-app/tailwind.config.js
+++ tahajjud-app/tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: ["Montserrrat", "sans-serif"],
+      backdropBlur: {
+        sm: '4px', 
+        md: '8px',
+        lg: '12px',
+      },
+    },
   },
   variants: {},
   plugins: [],
