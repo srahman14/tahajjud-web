12149764dfd4cb6705113fc8ed618a306b220017
srahman14 <s.rahman14@hotmail.com>
2025-02-27 04:49:34 +0000

    blog snippet component

---
+++ tahajjud-app/src/components/BlogSnippet.js
@@ -0,0 +1,28 @@
+// BlogSnippet.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { blogData  } from '../blogData';
+
+const BlogSnippet = () => {
+    const recentPost = blogData[0]
+    return (
+        <div className="bg-gray-100 p-8 rounded-lg flex flex-col w-full max-w-md mx-auto">
+        <h1 className="text-2xl font-semibold text-blue-700 mb-4">
+            {recentPost.title} (Snippet)
+        </h1>
+        <p className="font-semibold text-blue-700 mt-2 mb-8">
+            {recentPost.excerpt}
+        </p>
+        <p className='font-bold text-xl'>Tags:</p>
+        <div className="flex flex-wrap items-center">
+          {recentPost.tags.map((tag, index) => (
+            <span key={index} className="bg-gray-300 p-2 font-semibold hover:bg-gray-400 rounded-lg mr-2 mt-2">{tag} </span>
+          ))}
+        </div>
+
+        <Link to="/blog" className="text-blue-500 mt-4 mb-4">Read more</Link>
+        </div>
+    );
+};
+
+export default BlogSnippet;
