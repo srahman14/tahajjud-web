b56975d12709923a5c1dca784dec3290bba11494
srahman14 <s.rahman14@hotmail.com>
2025-02-28 01:32:38 +0000

    updated nav-menu and links

--- tahajjud-app/src/App.js
+++ tahajjud-app/src/App.js
@@ -17,37 +17,32 @@ function App() {
       <header className='bg-blue-600 text-white p-4'>
         <nav className='max-w-4xl mx-auto flex justify-between items-center'>
           <div className='flex items-center space-x'>
-          <a href='/' target='_blank' rel='noopener noreferrer'>
-            <img src='/tahajjud-logo.png' className='rounded-full w-20 md:w-12 mr-4'></img>
+          <a href='/' rel='noopener noreferrer'>
+            <img src='/tahajjud-logo.png' alt="tahajjud-times logo" className='rounded-full w-20 md:w-12 mr-4'></img>
           </a>
 
-          <h1 className='hidden md:block text-2xl font-bold mr-2'>Tahajjud</h1>
+          <h1 className='hidden md:block text-2xl font-bold mr-2'><a href='/' rel='noopener noreferrer'>Tahajjud</a></h1>
 
           </div>
           <ul className='flex space-x-6'>
-            <li className='p-1'><a href="/" className='hover:underline font-bold'>
+            <li className='p-1'><a href="/" rel='noopener noreferrer' className='hover:underline font-bold'>
                   <i className="fas fa-home text-3xl md:text-2xl lg:text-2xl"></i>
                   <span className="hidden md:inline ml-2">Home</span>
                 </a>
             </li>
-            <li className='p-1'><a href="/ramadan" className='hover:underline font-bold'>
+            <li className='p-1'><a href="/ramadan" rel='noopener noreferrer' className='hover:underline font-bold'>
                   <i className="fa-solid fa-moon text-3xl md:text-2xl lg:text-2xl"></i>
                   <span className="hidden md:inline ml-2">Ramadan</span>
                 </a>
             </li>
-            <li className='p-1'><a href="/tahajjud" className='hover:underline font-bold'>
+            <li className='p-1'><a href="/tahajjud" rel='noopener noreferrer' className='hover:underline font-bold'>
                   <i className="fa-solid fa-hands-praying text-3xl md:text-2xl lg:text-2xl"></i>
-                  <span className="hidden md:inline ml-2">Tahajjud</span>
+                  <span className="hidden md:inline ml-2">About Tahajjud</span>
                 </a>
             </li>
-            <li className='p-1'><a href="/blog" className='hover:underline font-bold'>
+            <li className='p-1'><a href="/blog" rel='noopener noreferrer' className='hover:underline font-bold'>
                   <i className="fa-solid fa-newspaper text-3xl md:text-2xl lg:text-2xl"></i>
                   <span className="hidden md:inline ml-2">Blog</span>
-                </a>
-            </li>
-            <li className='p-1'><a href="/creator" className='hover:underline font-bold'>
-                  <i className="fa-regular fa-circle-user text-3xl md:text-2xl lg:text-2xl"></i>
-                  <span className="hidden md:inline ml-2">Creator</span>
                 </a>
             </li>
           </ul>
@@ -69,17 +64,27 @@ function App() {
 
           <ul className='flex space-x-4 mt-2 sm:mt-0'>
             <li>
-              <a href="/" className='hover:underline'>
+              <a href="/" rel='noopener noreferrer' className='hover:underline'>
                 <span className="md:inline mt-2 mb-2">Home</span>
               </a>
             </li>
             <li>
-              <a href="/tahajjud" className='hover:underline'>
-                <span className="md:inline mt-2 mb-2">Tahajjud</span>
+              <a href="/tahajjud" rel='noopener noreferrer' className='hover:underline'>
+                <span className="md:inline mt-2 mb-2">About Tahajjud</span>
               </a>
             </li>
             <li>
-              <a href="/creator" className='hover:underline'>
+              <a href="/ramadan" rel='noopener noreferrer' className='hover:underline'>
+                <span className="md:inline mt-2 mb-2">Ramadan</span>
+              </a>
+            </li>
+            <li>
+              <a href="/blog" rel='noopener noreferrer' className='hover:underline'>
+                <span className="md:inline mt-2 mb-2">Blog</span>
+              </a>
+            </li>
+            <li>
+              <a href="/creator" rel='noopener noreferrer' className='hover:underline'>
                 <span className="md:inline mt-2 mb-2">Creator</span>
               </a>
             </li>
